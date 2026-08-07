6238e4c84a0cbc2ed2d2e6e7a1572272689efa6d
clarkxkent <astomilov@gmail.com>
2026-08-07 19:05:22 +0500

    Update checkdns.sh

--- checkdns.sh
+++ checkdns.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #set -x
 
-DOMAIN="${1:-store.epicgames.com}"
+DOMAIN="${1:-www.nvidia.com}"
 
 if ! command -v dig >/dev/null 2>&1; then
     echo "dig is not installed. Commands to install:"
