989e9a2e7eb65a08ef3b5b79b1830b43b0efcdb5
clarkxkent <astomilov@gmail.com>
2026-08-27 09:17:39 +0500

    Update poison_check.sh

--- poison_check.sh
+++ poison_check.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 # Список проверяемых публичных DNS серверов
-DNS_SERVERS="8.8.8.8 1.1.1.1 77.88.8.8 9.9.9.9 208.67.222.222 94.140.14.14"
+DNS_SERVERS="8.8.8.8 1.1.1.1 9.9.9.9 45.90.28.80 76.76.2.0 223.5.5.5 77.88.8.8 208.67.222.222 94.140.14.14"
 
 get_server_name() {
     case "$1" in
         8.8.8.8) echo "Google-DNS" ;;
         1.1.1.1) echo "Cloudflare" ;;
+        45.90.28.80) echo "NextDNS" ;;
+        76.76.2.0) echo "ControlD" ;;
+        223.5.5.5) echo "AliDNS" ;;
         77.88.8.8) echo "Yandex-Basic" ;;
         9.9.9.9) echo "Quad9-Secure" ;;
         208.67.222.222) echo "Cisco-OpenDNS" ;;
-        94.140.14.14) echo "AdGuard-Def" ;;
+        94.140.14.14) echo "AdGuard-Default" ;;
         *) echo "Unknown" ;;
     esac
 }
@@ -29,8 +32,8 @@ for SERVER_IP in $DNS_SERVERS; do
         IP_PUBLIC=$(dig +short @"$SERVER_IP" "$DOMAIN" A +time=2 +tries=1 2>/dev/null | grep -E '^[0-9.]+$' | head -n1)
 
         # 2. Получение ЧИСТОГО эталона в РФ через альтернативные порты, где нет перехвата ТСПУ
-        # Попытка 1: Quad9 
-        IP_TRUE=$(dig +short @9.9.9.9 +https "$DOMAIN" A +time=2 +tries=1 2>/dev/null | grep -E '^[0-9.]+$' | head -n1)
+        # Попытка 1: NextDNS
+        IP_TRUE=$(dig +short @45.90.28.92 +https "$DOMAIN" A +time=2 +tries=1 2>/dev/null | grep -E '^[0-9.]+$' | head -n1)
 
         # Попытка 2: Яндекс.Базовый (как резерв для незаблокированных в РФ сайтов)
         if [ -z "$IP_TRUE" ]; then
