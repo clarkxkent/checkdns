9e93b05959a4f281af6398500814e2f96a3e27c4
clarkxkent <astomilov@gmail.com>
2026-08-31 16:13:01 +0500

    Update checkdns.sh

--- checkdns.sh
+++ checkdns.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #set -x
 
-DOMAIN="${1:-www.instagram.com}"
+DOMAIN="${1:-youtube.com}"
 
 if ! command -v dig >/dev/null 2>&1; then
     echo "dig is not installed. Commands to install:"
